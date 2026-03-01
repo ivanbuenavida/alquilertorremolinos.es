4c0118f1d8ac9391b71c9a6063f84b2a7202b998
Iván Pedro Benavides Matillas <pedrobenavidesmatillas@gmail.com>
2026-03-01 01:22:48 +0100

    x

--- src/components/property-details.ts
+++ src/components/property-details.ts
@@ -23,10 +23,10 @@ export class PropertyDetails extends LitElement {
       <!-- Header -->
       <div class="mb-5">
         <h1 class="display-4 fw-bold mb-3 text-dark">${TranslationService.l.hero_title}</h1>
-        <p class="text-secondary fs-5 d-flex align-items-center gap-2">
+        <a href="https://www.google.es/maps/place/36%C2%B037'26.6%22N+4%C2%B029'55.0%22W/@36.6240061,-4.4986878,3a,75y,116.03h,96.76t/data=!3m8!1e1!3m6!1sCIABIhA86cI1tJLhg2xzb0l3RATT!2e10!3e11!6shttps:%2F%2Flh3.googleusercontent.com%2Fgpms-cs-s%2FAFfmt2al4HgBHCoJQRpoZCfVYSs4TfsFs7jPmOLPz6o3o3JEpsUL4n1Xg_EfnaibYaJ7yTx3SD3bLlsN7ZaUCIh2Z_8Ko_awhsbGxNSOP1TYkqgjkUUIZVjh_WfieFatJUVO4g-El3KgRGJuqX-n%3Dw900-h600-k-no-pi-6.756274244445322-ya253.22037494810303-ro0-fo100!7i8192!8i4096!4m5!3m4!8m2!3d36.6240556!4d-4.4986111!10e5" target="_blank" rel="noopener noreferrer" class="text-decoration-none text-secondary fs-5 d-flex align-items-center gap-2">
           <i class="bi bi-geo-alt-fill text-primary"></i> 
           <span>${this.data.location}</span>
-        </p>
+        </a>
       </div>
 
       <!-- Host Card -->
